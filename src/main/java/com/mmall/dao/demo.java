75877e55dd675329c2162399b18bd649d8cc0aca
Water-Yang <sunaimin1993@163.com>
2019-04-02 19:35:55 +0800

    create demo

---
+++ src/main/java/com/mmall/dao/demo.java
@@ -0,0 +1,11 @@
+package com.mmall.dao;
+
+/**
+ * @Author: Sun
+ * @Date: 2019/4/2 19:34
+ */
+public class demo {
+    public static void main(String[] args){
+        System.out.println("Helllo world!");
+    }
+}
